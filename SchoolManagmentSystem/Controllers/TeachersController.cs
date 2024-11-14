aa46ef22b96564a53b43537ae7cb1197056456af
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-11-14 20:21:48 +0500

    Update subjects and teachers controller

--- SchoolManagmentSystem/Controllers/TeachersController.cs
+++ SchoolManagmentSystem/Controllers/TeachersController.cs
@@ -1,7 +1,8 @@
-﻿using Domain.DTOs.Subject;
+﻿using Domain.Common;
 using Domain.DTOs.Teacher;
 using Domain.Interfaces.Services;
 using Domain.ResourceParameters;
+using Domain.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Domain.Controllers;
@@ -18,69 +19,69 @@ public class TeachersController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<TeacherDto>>> GetTeachersAsync(
+    public async Task<Result<GetBaseResponse<TeacherDto>>> GetTeachersAsync(
         [FromQuery] TeacherResourceParameters teacherResourceParameters)
     {
         var teachers = await _teacherService.GetTeachersAsync(teacherResourceParameters);
-        return Ok(teachers);
+        return new Result<GetBaseResponse<TeacherDto>>(teachers);
     }
 
     [HttpGet("top5highestmarks")]
-    public async Task<ActionResult<IEnumerable<TeacherDto>>> GetTeachersForTop5StudentsWithHighestMarksAsync()
+    public async Task<Result<List<string>>> GetTeachersForTop5StudentsWithHighestMarksAsync()
     {
         var teachers = await _teacherService.GetTeachersForTop5StudentsWithHighestMarksAsync();
-        return Ok(teachers);
+        return new Result<List<string>>(teachers);
     }
 
     [HttpGet("top5lowestmarks")]
-    public async Task<ActionResult<IEnumerable<TeacherDto>>> GetTeachersForTop5StudentsWithLowestMarksAsync()
+    public async Task<Result<List<string>>> GetTeachersForTop5StudentsWithLowestMarksAsync()
     {
         var teachers = await _teacherService.GetTeachersForTop5StudentsWithLowestMarksAsync();
-        return Ok(teachers);
+        return new Result<List<string>>(teachers);
     }
 
     [HttpGet("{teacherId}/subjects")]
-    public async Task<ActionResult<IEnumerable<SubjectDto>>> GetSubjectsByTeacherIdAsync(int teacherId)
+    public async Task<Result<List<string>>> GetSubjectsByTeacherIdAsync(int teacherId)
     {
         var subjects = await _teacherService.GetSubjectsByTeacherIdAsync(teacherId);
 
-        return Ok(subjects);
+        return new Result<List<string>>(subjects);
     }
 
     [HttpGet("{id}", Name = "GetTeacherById")]
-    public async Task<ActionResult<TeacherDto>> GetTeacherByIdAsync(int id)
+    public async Task<Result<TeacherDto>> GetTeacherByIdAsync(int id)
     {
         var teacher = await _teacherService.GetTeacherByIdAsync(id);
 
-        return Ok(teacher);
+        return new Result<TeacherDto>(teacher);
     }
 
     [HttpPost]
-    public async Task<ActionResult<TeacherDto>> PostAsync([FromBody] TeacherCreateDto teacherCreateDto)
+    public async Task<Result<TeacherDto>> PostAsync([FromBody] TeacherCreateDto teacherCreateDto)
     {
         var createdTeacher = await _teacherService.CreateTeacherAsync(teacherCreateDto);
 
-        return CreatedAtAction("GetTeacherById", new { id = createdTeacher.Id }, createdTeacher);
+        return new Result<TeacherDto>(createdTeacher);
     }
 
     [HttpPut("{id}")]
-    public async Task<ActionResult<TeacherDto>> PutAsync(int id, [FromBody] TeacherUpdateDto teacherUpdateDto)
+    public async Task<Result<TeacherDto>> PutAsync(int id, [FromBody] TeacherUpdateDto teacherUpdateDto)
     {
         if (id != teacherUpdateDto.Id)
         {
-            return BadRequest($"Route id: {id} does not match with parameter id: {teacherUpdateDto.Id}.");
+            return new Result<TeacherDto>($"Route id: {id} does not match with parameter id: {teacherUpdateDto.Id}.");
         }
 
         var updatedTeacher = await _teacherService.UpdateTeacherAsync(teacherUpdateDto);
 
-        return Ok(updatedTeacher);
+        return new Result<TeacherDto>(updatedTeacher);
     }
 
     [HttpDelete("{id}")]
-    public async Task<ActionResult> DeleteAsync(int id)
+    public async Task<Result<string>> DeleteAsync(int id)
     {
         await _teacherService.DeleteTeacherAsync(id);
 
-        return NoContent();
+        return new Result<string>("Teacher successfully deleted");
     }
 }
